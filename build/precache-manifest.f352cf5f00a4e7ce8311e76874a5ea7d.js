5be283711b2e784e54bd72b400ac190d754f85bf
aniruddh-9 <aniruddh9.m@gmail.com>
2020-06-13 16:27:58 +0530

    gh-pages

---
+++ build/precache-manifest.f352cf5f00a4e7ce8311e76874a5ea7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3da442b5c4ce1fd953d3d2a11e741b88",
+    "url": "/monster-book/index.html"
+  },
+  {
+    "revision": "1a368a25c62324f57708",
+    "url": "/monster-book/static/css/main.9a2a8a1a.chunk.css"
+  },
+  {
+    "revision": "fb217e93f0c63eaafdf6",
+    "url": "/monster-book/static/js/2.254c615a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-book/static/js/2.254c615a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a368a25c62324f57708",
+    "url": "/monster-book/static/js/main.0bbfd929.chunk.js"
+  },
+  {
+    "revision": "e83513dba663e0a6eea1",
+    "url": "/monster-book/static/js/runtime-main.9dedfe32.js"
+  }
+]);
