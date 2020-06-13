4898078b771de3351d48162221677b796d58af55
aniruddh-9 <aniruddh9.m@gmail.com>
2020-06-13 16:15:45 +0530

    CSS done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component{
   constructor(){
-    super();
+    super(); 
 
     this.state = {
       monsters : [],
@@ -18,6 +19,10 @@ class App extends Component{
     .then(users => this.setState({monsters :users}));
   }
 
+  handleChange = (e) => (                         // Arrow function avoids binding 'this' explicitly
+    this.setState({searchField : e.target.value})
+  )
+
   render(){
       // Desctructuring state
       const { monsters, searchField } = this.state;
@@ -28,10 +33,11 @@ class App extends Component{
     console.log(this.state.monsters);
     return(
       <div className='App'>
-        <input 
-        type='search' 
-        placeholder='Search Monster'
-        onChange={e => this.setState({searchField : e.target.value})}></input>
+        <h1>Monsters Gallery</h1>
+        <SearchBox
+          placeholder='Search Monster'
+          handleChange={this.handleChange}
+        />
         <CardList monsters={filteredMonsters} />
       </div>
     ); 
